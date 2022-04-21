4fe8edb157d89150eb2ceaad30aa102b039203ce
fahmi-aa <88307266+fahmi-aa@users.noreply.github.com>
2022-04-21 22:12:14 +0700

    add: files

---
+++ kafka-basics/src/main/java/io/fahmi/demos/kafka/ProducerDemo.java
@@ -0,0 +1,34 @@
+package io.fahmi.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Hello world");
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+
+        producer.send(producerRecord);
+
+        producer.flush();
+
+        // flush included in close
+        producer.close();
+    }
+}
